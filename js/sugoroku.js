5e49f8ad1f6b3e2313a59d9345d47e2c951d1062
pan24 <panab64@hotmail.co.jp>
2019-10-21 00:37:21 +0900

    init

---
+++ js/sugoroku.js
@@ -0,0 +1,5 @@
+"use strict";
+let message = '';
+message = "TypeScript World";
+const element = document.getElementById('text');
+element.textContent = message;
---
+++ js/sugoroku.ts
@@ -0,0 +1,4 @@
+let message : string = '';
+message = "TypeScript World"
+const element : HTMLElement  = <HTMLElement>document.getElementById('text');
+element.textContent = message;
